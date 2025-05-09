4e24ab006d8bab0ce45210247d2d0cdbc7022888
hyuuguisuo <guihnq@gmail.com>
2025-05-09 13:45:47 -0300

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = '1234567890'
 @app.route('/')
 
 def home():
-    return render_template('index.html')
+    return render_template('calcula2000.html')
 
 @app.route('/valor', methods=["POST"])
 
