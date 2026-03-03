a5f3e4146c225650a4257421afbfdca16e2da489
hyuuguisuo <guihnq@gmail.com>
2026-03-03 17:50:46 -0300

    novo teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarParaAngulos() {
+    document.getElementById('section-lados').style.display = 'none';
+    document.getElementById('section-angulos').style.display = 'flex';
+}
+
+function mudarParaLados() {
+    document.getElementById('section-angulos').style.display = 'none';
+    document.getElementById('section-lados').style.display = 'flex';
+}
